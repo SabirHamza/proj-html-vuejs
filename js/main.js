37757e1dd4cc014e356b90c0e7d0248fa7f5fff1
SabirHamza <h.sabir.ma@gmail.com>
2023-02-22 12:04:37 +0100

    reviews

--- js/main.js
+++ js/main.js
@@ -30,21 +30,47 @@ const app = createApp ({
                 {
                     img: './img/8wa60okr-1-790x576.jpg',
                     text: 'Basket of flower on table',
-                    type: 'Digital Strategy',
-                    class: 'point'
+                    type: 'Digital Strategy'
                 },
                 {
                     img: './img/84316050-0af0-49db-a53a-241d47ddad0e-2-790x576.jpg',
                     text: 'Purinky Product',
-                    type: 'Digital Strategy',
-                    class: 'point'
+                    type: 'Digital Strategy'
                 },
                 {
                     img: './img/a247b00b-3621-470f-b4b8-b3ac46f25907-1-790x576.jpg',
                     text: 'Satisfy Poster',
-                    type: 'Digital Strategy',
-                    class: 'point'
+                    type: 'Digital Strategy'
                 }
+            ],
+
+            review: [
+                {
+                    img: './img/philipe-cavalcante-539505-unsplash-1024x1024.png',
+                    text: 'when, while the lovely valley teems whit vapour around meand the meridian sun strikes the upper surface',
+                    client: 'Michekl Anegel',
+                    rule: 'Marketing Management'
+                },
+                {
+                    img: './img/szabo-viktor-1266895-unsplash-1024x1024.png',
+                    text: 'when, while the lovely valley teems whit vapour around meand the meridian sun strikes the upper surface',
+                    client: 'Michekl Anegel',
+                    rule: 'Marketing Management'
+                },
+                {
+                    img: './img/fabio-spinelli-695744-unsplash-1024x1024.png',
+                    text: 'when, while the lovely valley teems whit vapour around meand the meridian sun strikes the upper surface',
+                    client: 'Michekl Anegel',
+                    rule: 'Marketing Management'
+                }
+            ],
+
+            partners: [
+                './img/client-1-1.png',
+                './img/client-5.png',
+                './img/clienty-2.png',
+                './img/clienty-3.png',
+                './img/clienty-4.png'
             ]
         }
     }
